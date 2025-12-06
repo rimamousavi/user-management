0e6bf12959bd313db408973e04eac7afc289ee33
rimabmousavi <rimamousavi.team@gmail.com>
2025-12-06 11:41:34 +0330

    fixed: tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '2rem',
+      },
+      // screens: {
+      //   sm: '600px',
+      //   md: '728px',
+      //   lg: '984px',
+      //   xl: '1240px',
+      //   '2xl': '1496px',
+      // },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'gray-light': '#f2f2f5',
+      },
+    },
   },
   plugins: [],
 }
