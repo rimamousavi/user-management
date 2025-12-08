2d4dbd7503b66cd76b09eee77dc5a1488b017bb5
rimamousavi <rimamousavi.team@gmail.com>
2025-12-08 15:00:26 +0330

    change

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: "/",
+    publicPath: "/user-management/",
   },
   module: {
     rules: [
